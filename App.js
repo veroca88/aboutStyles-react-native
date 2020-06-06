3bc629505243199eab2e08479341c5fd11e674f0
veroca88 <veronicadonoso1506@gmail.com>
2020-06-06 15:01:33 -0400

    Example encapsulating text

--- App.js
+++ App.js
@@ -5,11 +5,24 @@ import Flex from "./app/screens/aboutFlexbox";
 import Shadows from "./app/screens/aboutShadows";
 import PaddingMargin from "./app/screens/aboutPaddingMargin";
 import AboutText from "./app/screens/aboutText";
+import AppText from "./app/screens/aboutEncapsulatingText";
 
 export default function App() {
   // return <Styles />;
   // return <Flex />;
   // return <Shadows />;
   // return <PaddingMargin />;
-  return <AboutText />;
+  // return <AboutText />;
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <AppText>I love love love React Native!</AppText>
+    </View>
+  );
 }
---
+++ app/screens/aboutEncapsulatingText.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Text, StyleSheet, Platform } from "react-native";
+
+//Common used to headers
+
+function AppText({ children }) {
+  return <Text style={styles.text}>{children}</Text>;
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 18,
+    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
+  },
+});
+
+export default AppText;
