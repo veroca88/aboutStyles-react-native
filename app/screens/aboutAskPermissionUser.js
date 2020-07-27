eeb09ab2d2534aec37c65bb20d033074d8fee5bc
veroca88 <veronicadonoso1506@gmail.com>
2020-07-27 18:13:49 -0400

    Add async function to ask permision to location too

--- app/screens/aboutAskPermissionUser.js
+++ app/screens/aboutAskPermissionUser.js
@@ -1,10 +1,14 @@
-import React, { useEffect } from "react";
-import { StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Button } from "react-native";
 import Screen from "./Screen";
 import * as ImagePicker from "expo-image-picker";
+import * as Permission from "expo-permissions";
 
 function aboutAskPermissionUser(props) {
+const [imageUri, setImageUri] = useState()
+
   const requestPermission = async () => {
+    const result = await Permission.askAsync(Permission.CAMERA_ROLL, Permission.LOCATION)
     const result = await ImagePicker.requestCameraRollPermissionsAsync();
     if (!result.granted)
       alert("You need to enable permission to access the library");
@@ -14,7 +18,20 @@ function aboutAskPermissionUser(props) {
     requestPermission();
   }, []);
 
-  return <Screen></Screen>;
+  const selectImage = async() => {
+      try {
+          const result = await ImagePicker.launchImageLibraryAsync()
+          if(!result.cancelled)
+      } catch (error) {
+          console.log('Error reading an image')
+          
+      }
+  }
+
+  return <Screen>
+      {/* Accessing the library */}
+      <Button title="Select Image" onPress={selectImage}></Button>
+  </Screen>;
 }
 
 const styles = StyleSheet.create({
