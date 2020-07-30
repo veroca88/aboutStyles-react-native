413613ea725b725fce875daa77af1344c73f8906
veroca88 <veronicadonoso1506@gmail.com>
2020-07-30 10:23:01 -0400

    Ask permission to select image in library

--- app/screens/aboutAskPermissionUser.js
+++ app/screens/aboutAskPermissionUser.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Button } from "react-native";
+import { StyleSheet, Button, Image } from "react-native";
 import Screen from "./Screen";
 import * as ImagePicker from "expo-image-picker";
 import * as Permission from "expo-permissions";
 
 function aboutAskPermissionUser(props) {
-const [imageUri, setImageUri] = useState()
+  const [imageUri, setImageUri] = useState();
 
   const requestPermission = async () => {
-    const result = await Permission.askAsync(Permission.CAMERA_ROLL, Permission.LOCATION)
+    // const result = await Permission.askAsync(
+    //   Permission.CAMERA_ROLL,
+    //   Permission.LOCATION
+    // );
     const result = await ImagePicker.requestCameraRollPermissionsAsync();
     if (!result.granted)
       alert("You need to enable permission to access the library");
@@ -18,24 +21,22 @@ const [imageUri, setImageUri] = useState()
     requestPermission();
   }, []);
 
-  const selectImage = async() => {
-      try {
-          const result = await ImagePicker.launchImageLibraryAsync()
-          if(!result.cancelled)
-      } catch (error) {
-          console.log('Error reading an image')
-          
-      }
-  }
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.cancelled) setImageUri(result.uri);
+    } catch (error) {
+      console.log("Error reading an image");
+    }
+  };
 
-  return <Screen>
+  return (
+    <Screen>
       {/* Accessing the library */}
       <Button title="Select Image" onPress={selectImage}></Button>
-  </Screen>;
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+    </Screen>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {},
-});
 
 export default aboutAskPermissionUser;
